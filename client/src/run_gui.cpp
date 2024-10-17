713eb9608c842f45888b9c481a16b1d7a9ae8791
Nicolas Alexandre <114945623+Nicolasalx@users.noreply.github.com>
2024-10-17 19:12:42 +0200

    add: title menu has been added with a button play, options and quit (#188)
    * add: title menu has been added with a button play, options and quit
    
    * mod: fix format in rtype client header
    
    * fix: clang tidy have been fixed in the title menu
    
    * mod: fix format in rtype client header
    
    * fix: enum class windowMode has been fixed in the title menu

--- client/src/run_gui.cpp
+++ client/src/run_gui.cpp
@@ -13,92 +13,18 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 
-static void renderInsideRoom(rtc::RoomManager &roomManager, const sf::Vector2u &windowSize)
-{
-    // ! Window
-    ImGui::SetNextWindowSize(ImVec2(windowSize.x, windowSize.y));
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::Begin(
-        roomManager.getCurrentRoom().c_str(),
-        nullptr,
-        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove
-    );
-
-    // ! Table
-    ImGui::BeginTable("playersTable", 2);
-    ImGui::TableSetupColumn("Name");
-    ImGui::TableSetupColumn("Status");
-    ImGui::TableHeadersRow();
-    ImU32 selfRowColor = ImGui::GetColorU32(ImVec4(0.5, 1, 0.5, 0.25));
-
-    // ! Action with table
-    for (const auto &[id, player] : roomManager.getCurrentRoomPlayer()) {
-        ImGui::TableNextRow();
-        if (id == roomManager.getSelfId()) {
-            ImGui::TableSetBgColor(ImGuiTableBgTarget_RowBg0, selfRowColor);
-        }
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("%s", player.name.c_str());
-        ImGui::TableSetColumnIndex(1);
-        ImGui::Text("%s", player.ready ? "ready" : "not ready");
-    }
-    ImGui::EndTable();
-
-    // ! Back to lobby button
-    {
-        ImVec2 buttonSize(200, 50);
-        ImVec2 windowContentRegionMax = ImGui::GetWindowContentRegionMax();
-        ImVec2 buttonPos =
-            ImVec2(windowContentRegionMax.x - buttonSize.x - 20, windowContentRegionMax.y - buttonSize.y - 20);
-        ImGui::SetCursorPos(buttonPos);
-        if (ImGui::Button("Back to lobby", buttonSize)) {
-            // ! send leave
-            roomManager.askToLeaveRoom();
-        }
-    }
-
-    // ! ready button
-    ImVec2 buttonSize(200, 50);
-    ImVec2 windowContentRegionMax = ImGui::GetWindowContentRegionMax();
-    ImVec2 buttonPos = ImVec2(20, windowContentRegionMax.y - buttonSize.y - 20);
-    ImGui::SetCursorPos(buttonPos);
-    if (!roomManager.getCurrentRoomPlayer().at(roomManager.getSelfId()).ready) {
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 1.0f, 0.0f, 1.0f));
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.0f, 0.6f, 0.0f, 1.0f));
-        if (ImGui::Button("Ready", buttonSize)) {
-            roomManager.askToBeReady();
-        }
-    } else {
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1.0f, 0.0f, 0.0f, 1.0f));
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.8f, 0.0f, 0.0f, 1.0f));
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.6f, 0.0f, 0.0f, 1.0f));
-        if (ImGui::Button("Not Ready", buttonSize)) {
-            roomManager.askToBeNotReady();
-        }
-    }
-    ImGui::PopStyleColor(3);
-
-    ImGui::End();
-}
-
-static void unsupportedWindowSize(const sf::Vector2u &windowSize)
-{
-    ImGui::SetNextWindowSize(ImVec2(windowSize.x, windowSize.y));
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::Begin(
-        "Window to small",
-        nullptr,
-        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove
-    );
-    ImGui::Text("Window size not supported !");
-    ImGui::End();
-}
-
 void rtc::runGui(const std::shared_ptr<sf::RenderWindow> &window, rtc::RoomManager &roomManager, bool &inLobby)
 {
     sf::Clock dt;
     sf::Vector2u windowSize;
+    WindowMode windowMode = rtc::WindowMode::MENU;
+    int fpsLimit = rt::CLIENT_FPS_LIMIT;
+
+    ImGuiIO &io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("assets/font/DroidSansMono.ttf", 32.0f);
+    if (!ImGui::SFML::UpdateFontTexture()) {
+        return;
+    };
 
     while (window->isOpen() && inLobby) {
         sf::Event event{};
@@ -110,14 +36,14 @@ void rtc::runGui(const std::shared_ptr<sf::RenderWindow> &window, rtc::RoomManag
         }
         ImGui::SFML::Update(*window, dt.restart());
         window->clear();
-
         windowSize = window->getSize();
-        if (windowSize.x < rt::MIN_SCREEN_WIDTH || windowSize.y < rt::MIN_SCREEN_HEIGHT) {
-            unsupportedWindowSize(windowSize);
-        } else if (roomManager.getCurrentRoom().empty()) {
-            rtc::renderLobbyWindow(roomManager, windowSize);
+
+        if (windowMode == rtc::WindowMode::MENU) {
+            menuWindow(*window, io, windowMode);
+        } else if (windowMode == rtc::WindowMode::OPTIONS) {
+            optionsWindow(*window, windowSize, fpsLimit, windowMode);
         } else {
-            renderInsideRoom(roomManager, windowSize);
+            lobbyWindow(windowSize, roomManager);
         }
 
         ImGui::SFML::Render(*window);
