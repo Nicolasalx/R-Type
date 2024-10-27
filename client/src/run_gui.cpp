0728f751638883ababe603269b72e8303bb45ca7
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-10-27 19:33:15 +0100

    add: sfx ui has been added, now when user click, we can hear a sound

--- client/src/run_gui.cpp
+++ client/src/run_gui.cpp
@@ -23,6 +23,7 @@ void rtc::runGui(
     ecs::KeyBind<rt::PlayerAction, sf::Keyboard::Key> &keyBind
 )
 {
+    ecs::SoundManager soundManager;
     sf::Clock dt;
     sf::Vector2u windowSize;
     WindowMode windowMode = rtc::WindowMode::MENU;
@@ -47,6 +48,10 @@ void rtc::runGui(
                 chatEnable = !chatEnable;
             } else if (event.type == sf::Event::Resized) {
                 windowSize = window->getSize();
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                soundManager.loadSoundBuffer("ui_click", "assets/uiClick.wav");
+                soundManager.playSoundEffect("ui_click", 100.f, false);
             }
         }
         ImGui::SFML::Update(*window, dt.restart());
