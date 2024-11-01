3ba4e04ba04da30576fee054e0d4f07283980508
ManuelR-T <105550975+ManuelR-T@users.noreply.github.com>
2024-11-01 22:17:31 +0000

    Deploying to gh-pages from @ ManuelR-T/R-Type@7020207dd98e9c4bba164e3e0dc5ff57b63b4ecc 🚀

--- search/all_17.js
+++ search/all_17.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['x_0',['x',['../db/d8f/structecs_1_1component_1_1Position.html#ae9c867a6647b7667184497b51fecdfb5',1,'ecs::component::Position::x()'],['../dc/d1b/structecs_1_1component_1_1SubSprite.html#ac9c77e99fbbab628019a9f3e8bc0a56b',1,'ecs::component::SubSprite::x()']]]
+  ['x_0',['x',['../db/d8f/structecs_1_1component_1_1Position.html#ae9c867a6647b7667184497b51fecdfb5',1,'ecs::component::Position::x()'],['../d3/d56/structecs_1_1component_1_1OnDeath.html#ac99e4bf161b6a06eb9b16d74c03752ea',1,'ecs::component::OnDeath::x()'],['../dc/d1b/structecs_1_1component_1_1SubSprite.html#ac9c77e99fbbab628019a9f3e8bc0a56b',1,'ecs::component::SubSprite::x()']]]
 ];
--- search/all_18.js
+++ search/all_18.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['y_0',['y',['../db/d8f/structecs_1_1component_1_1Position.html#ac9a7a6e991d800ddd4b79bbdd31fef9d',1,'ecs::component::Position::y()'],['../dc/d1b/structecs_1_1component_1_1SubSprite.html#a31b53d0f2528333b74a68945aab6d6bc',1,'ecs::component::SubSprite::y()']]]
+  ['y_0',['y',['../db/d8f/structecs_1_1component_1_1Position.html#ac9a7a6e991d800ddd4b79bbdd31fef9d',1,'ecs::component::Position::y()'],['../d3/d56/structecs_1_1component_1_1OnDeath.html#a335f4d8dee5a8ac4cebd1fa94870d2f4',1,'ecs::component::OnDeath::y()'],['../dc/d1b/structecs_1_1component_1_1SubSprite.html#a31b53d0f2528333b74a68945aab6d6bc',1,'ecs::component::SubSprite::y()']]]
 ];
